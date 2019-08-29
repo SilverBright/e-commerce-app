097de0deba4160a425c8682fe51466d9229a2511
github username <barbara.m.washington@gmail.com>
2019-08-29 12:43:25 -0700

    create Product.js with boilerplate code

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Product from './';
+import Product from './Product';
 
 export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from ProductList.js</h3>
+       <Product />
       </div>
     )
   }

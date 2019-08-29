5fb28055d483c83330cb2c83f0e33b26ed3007ec
github username <barbara.m.washington@gmail.com>
2019-08-29 14:07:05 -0700

    import components into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
     // use fragments to create components that return a list of elements without wrapping them in a container or array
     <React.Fragment>
-      <h1>Hello from App.js</h1>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
     </React.Fragment>
   );
 }

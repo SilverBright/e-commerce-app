69311ed9d74467ed00b3384d0995b5880bb36fcc
github username <barbara.m.washington@gmail.com>
2019-09-04 21:44:49 -0700

    add title and some styling to ProductsList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import{ storeProducts } from '../data';
 
 export default class ProductList extends Component {
+  state = {
+    products: []
+  }
+
   render() {
+
     return (
-      <div>
-       <Product />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="my" title="products" />
+
+            <div className="row">
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
